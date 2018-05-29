b31e88cf64cf11a67a5c09cb99a29eaaaa4c6ddb
yuekongxingyuan <2472029611@qq.com>
2018-05-29 16:51:40 +0800

    offer66题

---
+++ Offer66/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+/*in.nextLine()读取一行数据，包括空格
+ *in.nextInt()读取一个int数据，加个for循环可以一直读取很多数据直到for结束 ，注意如果输入数据此时不是int会抛出异常
+ *in.nextDouble()
+ *in.next()读取一个单词，以空格为分隔符
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
